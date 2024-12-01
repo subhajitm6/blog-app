72c0dd9b94b8645205d7f3d1a2ffa3af52d69e66
Subhajit Manna <subhajitmanna264@gmail.com>
2024-12-01 22:26:11 +0530

    env file added

--- app/admin/addProduct/page.jsx
+++ app/admin/addProduct/page.jsx
@@ -55,7 +55,7 @@ const page = () => {
         <form onSubmit={onSubmitHandler} className='pt-5 px-5 sm:pt-12 sm:pl-16'>
             <p className='text-xl'>Upload thumbnail</p>
             <label htmlFor="image">
-                <Image className='mt-4' src={!image ? assets.upload_area : URL.createObjectURL(image)} width={140} height={70}/>
+                <Image className='mt-4' alt='' src={!image ? assets.upload_area : URL.createObjectURL(image)} width={140} height={70}/>
             </label>
             <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='image' hidden required/>
             <p className='text-xl mt-4'>Blog title</p>
